593f1b9a7c6c366cf3c30602d1bc773cfe94e0cc
Stephen Redeker <sredeker@sapient.com>
2019-06-20 13:27:58 -0400

    add scss compiler and foundation library

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: $body-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: get-color(black);
   margin-top: 60px;
 }
 </style>
