90d5c38bef516650831c9ec20310c7c036dd6d40
Jarod Raedels <jarod.raedels@publicissapient.com>
2019-06-25 16:13:56 -0400

    Home page, shirts, cart work and categories page with nothing in it

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Shirt1 from '@/components/Shirt1'
+import Shirt2 from '@/components/Shirt2'
+import Shirt3 from '@/components/Shirt3'
+import Cart from '@/components/Cart'
+import Categories from '@/components/Categories'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/shirt1',
+      name: 'Shirt1',
+      component: Shirt1
+    },
+    {
+      path: '/shirt2',
+      name: 'Shirt2',
+      component: Shirt2
+    },
+    {
+      path: '/shirt3',
+      name: 'Shirt3',
+      component: Shirt3
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/categories',
+      name: 'Categories',
+      component: Categories
     }
   ]
 })
