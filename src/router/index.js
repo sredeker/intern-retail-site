227e71fa07d19bba394a581da96c5cb0b127a2b4
Jarod Raedels <jarod.raedels@publicissapient.com>
2019-06-28 09:03:31 -0400

    Removed useless shirt.vue files

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Shirt1 from '@/components/Shirt1'
-import Shirt2 from '@/components/Shirt2'
-import Shirt3 from '@/components/Shirt3'
 import Cart from '@/components/Cart'
 import Categories from '@/components/Categories'
 import ProductManagement from '@/components/ProductManagement'
@@ -20,21 +17,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: '/shirt1',
-      name: 'Shirt1',
-      component: Shirt1
-    },
-    {
-      path: '/shirt2',
-      name: 'Shirt2',
-      component: Shirt2
-    },
-    {
-      path: '/shirt3',
-      name: 'Shirt3',
-      component: Shirt3
     },
     {
       path: '/cart',
